7dbb861804efa2c09683a9e563913dc08e430c4c
Jeff Orr <jefforr@mwc-020218.dhcp.missouri.edu>
2017-02-08 12:53:04 -0600

    Adding a comment

--- GitXcodeWorkflow/GitXcodeWorkflow/ViewController.swift
+++ GitXcodeWorkflow/GitXcodeWorkflow/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //COMMIT THEN PUSH
+    
     func sayHello() {
         print("Sup world!")
     }
